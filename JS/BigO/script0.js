ca69d4a6eccb376c96163b2e9533f2412e144d04
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-08-22 20:41:05 -0400

    O(n) - linear time

--- JS/BigO/script0.js
+++ JS/BigO/script0.js
@@ -13,22 +13,17 @@ const cars = [
   "mazda",
 ];
 
-const newArray = new Array(50).fill("tesla");
+const newArray = new Array(20).fill("honda");
 
 // function that receive an array
 function findCar(array) {
-  let timeStart = performance.now();
   for (let i = 0; i < array.length; i++) {
     // check if the array have the name honda
     if (array[i] === "honda") {
       console.log("Honda was found");
     }
   }
-  let timeEnd = performance.now();
-  console.log(
-    `The call to find the car took ${timeEnd - timeStart} milliseconds`
-  );
 }
 
 console.log(findCar(cars));
-findCar(newArray);
+findCar(newArray); // O(n) - linear time
