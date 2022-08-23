4515fd879b3532795cdc2cbe04f9f0230fce5b4d
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-08-23 11:12:58 -0400

    big o

--- JS/BigO/script0.js
+++ JS/BigO/script0.js
@@ -2,6 +2,7 @@
 // require performance
 const { performance } = require("perf_hooks");
 // define and array
+const honda = ["honda"];
 const cars = [
   "honda",
   "toyota",
@@ -19,11 +20,13 @@ const newArray = new Array(20).fill("honda");
 function findCar(array) {
   for (let i = 0; i < array.length; i++) {
     // check if the array have the name honda
+    console.log("running");
     if (array[i] === "honda") {
       console.log("Honda was found");
+      break;
     }
   }
 }
 
-console.log(findCar(cars));
-findCar(newArray); // O(n) - linear time
+// console.log(findCar(cars));
+findCar(cars); // O(n) - linear time
