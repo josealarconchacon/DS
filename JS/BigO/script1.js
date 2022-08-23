992d33255f71ec0b74a5ce3c6a82a0c87e0b7442
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-08-22 21:03:07 -0400

    big o

--- JS/BigO/script1.js
+++ JS/BigO/script1.js
@@ -7,3 +7,20 @@ function getFirstTwoItem(items) {
   console.log(items[1]); // O(1)
 }
 getFirstTwoItem(items); // O(2)
+
+// ----------------------------------------
+
+function funChallenge(input) {
+  let num = 10; // O(1)
+  num = 50 + 3; // O(1)
+
+  // O(n)
+  for (let i = 0; i < input.length; i++) {
+    Function(); // O(n)
+    let value = true; // O(n)
+    num++; // O(n)
+  }
+  return num; // O(1)
+}
+
+funChallenge(); // Big O(3 + 4n)
