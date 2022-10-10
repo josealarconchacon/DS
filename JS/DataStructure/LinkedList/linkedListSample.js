6ed3614a60309f1189a831ecd060262c033b455d
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-10-10 11:37:57 -0400

    prepend node to linkedList

--- JS/DataStructure/LinkedList/linkedListSample.js
+++ JS/DataStructure/LinkedList/linkedListSample.js
@@ -41,9 +41,25 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  // prepend element to the beginning of the LinkedList
+  prepend(value) {
+    const node = {
+      value: value,
+      next: null,
+    };
+    // add the new node to head
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+    return this;
+  }
 }
 
 const linkedList = new LinkedList(12);
+// append :  12 --> 5 --> 2
 linkedList.append(5);
 linkedList.append(2);
+// prepend 40 to:  40 --> 12 --> 5 --> 2
+linkedList.prepend(40);
 console.log(linkedList);
